debfd5c77cfc71f9324d8fc27cbb4dc4cbd371c8
Ishan Taunk <Ishan.Taunk@Quantum.com>
2023-02-03 18:08:33 +0530

    top menu animation added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,24 @@
 import Home from './pages/Home/Home';
 import Profile from './pages/Profile/Profile';
+import Components from './pages/Components/Components';
 import './App.css';
 import {BrowserRouter, Route, Routes, Link} from "react-router-dom";
-import { useNavigate } from "react-router-dom"; 
 
 function App() {
   return (
     <div className="App">
         <BrowserRouter>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/profile">Profile</Link></li>
+                <ul className='toolbar'>
+                    <li><div className='link'><Link to="/" data-item="Home">Home</Link></div></li>
+                    <li><div className='link'><Link to="/profile" data-item="Profile">Profile</Link></div></li>
+                    <li><div className='link'><Link to="/components" data-item="Components">Components</Link></div></li>
                 </ul>
             </nav>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/components" element={<Components />} />
             </Routes>
        </BrowserRouter>
     </div>
---
+++ my-app/src/pages/Components/Components.js
@@ -0,0 +1,7 @@
+const Components=()=>{
+    return <div>
+        This is Components Page.
+    </div>
+}
+
+export default Components;
